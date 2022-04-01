716b4381073b256f203f41798dceb7a2b8639124
Aakash Kumar Das <aakashjsr@gmail.com>
2022-04-02 01:43:39 +0530

    Update admin.py

--- django_admin_row_actions/admin.py
+++ django_admin_row_actions/admin.py
@@ -1,6 +1,6 @@
 from django import VERSION
 from django import forms
-from django.conf.urls import url
+from django.conf.urls import path
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
@@ -109,7 +109,7 @@ class AdminRowActionsMixin(object):
 
         my_urls = patterns(
             '',
-            url(r'^(?P<pk>[0-9a-f-]+)/rowactions/(?P<tool>\w+)/$',
+            path('<str:pk>/rowactions/<str:pk>/',
                 self.admin_site.admin_view(ModelToolsView.as_view(model=self.model))
             )
         )
