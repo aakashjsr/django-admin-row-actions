14d3326b136b4b44485308a8653f210077ea5752
Aakash Kumar Das <aakashjsr@gmail.com>
2022-04-02 01:44:37 +0530

    Update admin.py

--- django_admin_row_actions/admin.py
+++ django_admin_row_actions/admin.py
@@ -1,6 +1,6 @@
 from django import VERSION
 from django import forms
-from django.conf.urls import path
+from django.urls import path
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
